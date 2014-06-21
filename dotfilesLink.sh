9a1c63dcb53f401440c717ab27dca91303c685e4
y-kato <y-kato@mapion.co.jp>
2014-06-21 18:08:25 +0900

    ターミナルでのビープ音抑制

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -1,4 +1,5 @@
 #! /bin/bash
-ln -s ~/dotfiles/.vimrc ~/.vimrc
 ln -s ~/dotfiles/.gvimrc ~/.gvimrc
+ln -s ~/dotfiles/.inputrc ~/.inputrc
 ln -s ~/dotfiles/.vim ~/.vim
+ln -s ~/dotfiles/.vimrc ~/.vimrc
